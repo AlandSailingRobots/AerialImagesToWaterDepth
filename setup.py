c37ab6f6faa8e44264ffd80c97f9d8ef0a5fb534
TJ-P <47567077+TJ-P@users.noreply.github.com>
2019-05-03 08:37:58 +0300

    update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 
 setup(name='map_based_resources', version='0.0.1', packages=['map_based_resources'])
-setup(name='data_resource', version='0.0.1', packages=['data_resources'])
+setup(name='data_resources', version='0.0.1', packages=['data_resources'])
