06d22934cffe88e2e7e2aa35b57da82f71cd290c
TJ-P <47567077+TJ-P@users.noreply.github.com>
2019-09-16 18:28:23 +0200

    create script for queue of actions.

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -11,3 +11,11 @@ create table test.polygons
     zoom_level integer,
     geom       geometry
 );
+create table test.calculation
+(
+    index_key  serial not null,
+    payload    json   not null,
+    calculated boolean default false
+);
+
+comment on table test.calculation is 'The data which is needed for background calculation';
