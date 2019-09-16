34ea8b9f86d7c1a53bc4f6828839e5bc64b46009
TJ-P <47567077+TJ-P@users.noreply.github.com>
2019-09-16 18:30:34 +0200

    be able to run query in sequence instead of one at a time.

--- backend/DepthCalculationProcess.py
+++ backend/DepthCalculationProcess.py
@@ -7,6 +7,18 @@ cnn = ConvolutionalHandler(2)
 postGis = PostGisHandler()
 
 
+def calculate_per_frame(crs, df):
+    splitted = crs.split(':')[-1]
+    depths = cnn.predict_points(df.geom, crs)
+    df['depths'] = depths.flatten()
+    print(df.columns)
+    df['sql'] = df.apply(
+        lambda row: postGis.update_point_height(postGis.points_table, row.geom, splitted, row.depths,
+                                                row.zoom_level, return_query=True), axis=1)
+    query = "\n".join(list(df['sql']))
+    postGis.send_to_db(query)
+
+
 def calculate_per_single_point_update_database(crs, df):
     for index, item in df.iterrows():
         depths = cnn.predict_point(item.geom, crs)
