03cca8adf8526f0264ccaf5f41f38bc7a9198fb1
TJ-P <47567077+TJ-P@users.noreply.github.com>
2019-10-22 16:28:54 +0200

    clean up code

--- backend/Handlers/ConvolutionalNeuralNetworkHandler.py
+++ backend/Handlers/ConvolutionalNeuralNetworkHandler.py
@@ -18,11 +18,10 @@ class ConvolutionalHandler:
         super().__init__()
         self.model_config = fileToObjects.get_available_cnn_models()[model_config_index]
         if "model_path" in self.model_config:
-            self.model = load_model(fileToObjects.check_path(self.model_config["model_path"]),
-                                    custom_objects=fileToObjects.get_custom_objects_cnn_model())
+            path = fileToObjects.check_path(self.model_config["model_path"])
         else:
             path = fileToObjects.get_model_path(self.model_config, overwrite_backup=True)
-            self.model = load_model(path, custom_objects=fileToObjects.get_custom_objects_cnn_model())
+        self.model = load_model(path, custom_objects=fileToObjects.get_custom_objects_cnn_model())
         self.map_resource = MapResources()
 
     def get_image(self, longitude, latitude, epsg):
@@ -30,9 +29,6 @@ class ConvolutionalHandler:
                                epsg,
                                self.model_config["level"])
         image = self.map_resource.get_image(coordinate, specific=self.model_config)
-        # coordinate_tile = singleTile.get_image_and_plot(coordinate, self.map_resource, show=False,
-        #                                                 specific=self.model_config)
-        # image = coordinate_tile.get_cropped_image_single(self.model_config["size_in_meters"])
         if image.mode != 'RGBA':
             image = image.convert('RGBA')
         return np.array([np.array(image)])
