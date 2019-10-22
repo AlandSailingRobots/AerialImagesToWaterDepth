e48e3316e055140551aa0812d672891641cf40ec
TJ-P <47567077+TJ-P@users.noreply.github.com>
2019-10-22 11:21:48 +0200

    formatting and text fixes

--- backend/Process/PointCalculationProces.py
+++ backend/Process/PointCalculationProces.py
@@ -2,11 +2,11 @@ from time import sleep
 
 from backend.Handlers import GeoJsonHandler, PostGisHandler
 
-postgis = PostGisHandler()
-geojson = GeoJsonHandler()
+postgis = PostGisHandler.PostGisHandler()
+geojson = GeoJsonHandler.GeoJsonHandler()
 
 
-def calculatePoints():
+def calculate_points():
     data = postgis.select_from_table(postgis.calculation_table, where="calculated is FALSE", panda=True)
     if len(data) is 0:
         sleep(30)
@@ -15,6 +15,7 @@ def calculatePoints():
         geojson.calculateDepthPointsProces(item.payload)
         postgis.update_calculation(int(item.index_key))
 
+
 if __name__ == '__main__':
     while True:
-        calculatePoints()
+        calculate_points()
